e9fc4c385444a6aa675ae94aed08af1c4bc289f0
kota <52403688+yashikota@users.noreply.github.com>
2023-10-22 23:05:40 +0900

    add target number

--- unity/Assets/Scenes/MainScene/Scripts/Target.cs
+++ unity/Assets/Scenes/MainScene/Scripts/Target.cs
@@ -1,16 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Target : MonoBehaviour
 {
-    [SerializeField] private GameObject spriteObject;
-    [SerializeField] private float spriteScale = 0.025f;
+    GameObject circleObject;
     private Dictionary<int, Vector3> targets;
 
     void Start()
     {
-        Camera mainCamera = Camera.main;
+        circleObject = new GameObject("Circle");
 
+        SetCoordinate();
+        CreateCircleSprite();
+        GenerateSprites(1, 2, 3, 4);
+        GenerateSpritesNumber(new Dictionary<int, int> { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } });
+    }
+
+    private void SetCoordinate()
+    {
+        Camera mainCamera = Camera.main;
         const float center = 0.5f;
         const float offset = 0.15f;
 
@@ -31,20 +40,86 @@ public class Target : MonoBehaviour
             { 5, leftBottom },
             { 6, rightBottom }
         };
+    }
 
-        GenerateSprites(1, 3, 4);
+    private void CreateCircleSprite()
+    {
+        const float radius = 0.01f;
+        const int textureSize = 256;
+
+        SpriteRenderer spriteRenderer = circleObject.AddComponent<SpriteRenderer>();
+
+        Texture2D circleTexture = new(textureSize, textureSize)
+        {
+            filterMode = FilterMode.Bilinear,
+            wrapMode = TextureWrapMode.Clamp
+        };
+
+        for (int x = 0; x < circleTexture.width; x++)
+        {
+            for (int y = 0; y < circleTexture.height; y++)
+            {
+                if (Vector2.Distance(new Vector2(x, y), new Vector2(circleTexture.width / 2, circleTexture.height / 2)) <= circleTexture.width / 2)
+                {
+                    circleTexture.SetPixel(x, y, Color.white);
+                }
+                else
+                {
+                    circleTexture.SetPixel(x, y, Color.clear);
+                }
+            }
+        }
+        circleTexture.Apply();
+
+        spriteRenderer.sprite = Sprite.Create(circleTexture, new Rect(0, 0, circleTexture.width, circleTexture.height), Vector2.one * 0.5f);
+
+        circleObject.transform.localScale = new Vector3(radius, radius, 1.0f);
     }
 
     public void GenerateSprites(params int[] spriteNumbers)
     {
+        int spriteLayer = 0;
         foreach (int spriteNumber in spriteNumbers)
         {
             if (targets.ContainsKey(spriteNumber))
             {
-                Vector3 position = targets[spriteNumber];
-                GameObject sprite = Instantiate(spriteObject, position, Quaternion.identity);
-                sprite.transform.localScale = new Vector3(spriteScale, spriteScale, 1.0f);
-                sprite.name = "Sprite" + spriteNumber;
+                Vector3 targetPosition = targets[spriteNumber];
+                GameObject newCircle = Instantiate(circleObject, targetPosition, Quaternion.identity);
+                newCircle.name = "Target" + spriteNumber;
+
+                // Set layer
+                SpriteRenderer spriteRenderer = newCircle.GetComponent<SpriteRenderer>();
+                spriteRenderer.sortingOrder = spriteLayer;
+                spriteLayer++;
+            }
+        }
+    }
+
+    public void GenerateSpritesNumber(Dictionary<int, int> spriteNumbers)
+    {
+        int textLayer = 1;
+        foreach (KeyValuePair<int, int> spriteNumber in spriteNumbers)
+        {
+            if (targets.ContainsKey(spriteNumber.Key))
+            {
+                Vector3 targetPosition = targets[spriteNumber.Key];
+                GameObject textObject = new("Text")
+                {
+                    name = "Text" + spriteNumber.Key
+                };
+
+                TextMeshPro textMesh = textObject.AddComponent<TextMeshPro>();
+                textMesh.text = spriteNumber.Value.ToString();
+                textMesh.fontSize = 2.5f;
+                textObject.transform.position = targetPosition;
+                textObject.transform.localScale = new Vector3(0.1f, 0.1f, 1);
+                textMesh.color = new Color(0, 0, 0, 1);
+                textMesh.alignment = TextAlignmentOptions.Center;
+                textMesh.alignment = TextAlignmentOptions.Midline;
+
+                // Set layer
+                textMesh.sortingOrder = textLayer;
+                textLayer++;
             }
         }
     }
